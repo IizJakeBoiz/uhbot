51169ea494a0c5d0a7f87a138b9865b031499ff7
IizJakeBoiz <47463571+IizJakeBoiz@users.noreply.github.com>
2019-03-23 17:50:40 +0000

    Update Test.py

--- Test.py
+++ Test.py
@@ -2,8 +2,6 @@
 import discord
 from discord.ext import commands
 
-TOKEN = 'NTU5MDM5MDUwOTU5MjkwMzc4.D3fmGA.2zAs-gytNnH6QPJboHUI9yCW3S8'
-
 client = commands.Bot(command_prefix = '.')
 
 @client.event
@@ -11,4 +9,4 @@ async def on_ready():
     await client.change_presence(game=discord.Game(name='Protecting The Server'))
     print('Bot is ready.')
 
-client.run('NTU5MDM5MDUwOTU5MjkwMzc4.D3fmGA.2zAs-gytNnH6QPJboHUI9yCW3S8')
+client.run(TOKEN)
